d204b94d7ffd5280616186155d131f805a0cfb81
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2024-12-13 17:14:57 -0400

    model

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,32 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 import { Dialect } from "sequelize";
+import Formulario from "../models/Formulario.model";
+import FormularioRespondido from "../models/FormularioRespondido.model";
+import FormularioToken from "../models/FormularioToken.model";
+import Opcion from "../models/Opcion.model";
+import Pregunta from "../models/Pregunta.model";
+import RespuestaUsuario from "../models/RespuestaUsuario";
+import Rol from "../models/Rol.model";
+import Token from "../models/Token";
+import Usuario from "../models/Usuario.model";
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
     dialect: "postgres" as Dialect,
     logging: false,
-    models: [__dirname + "/../models/**/*.ts"],
+    //models: [__dirname + "/../models/**/*.ts"],
+    models: [
+        Formulario,
+        FormularioRespondido,
+        FormularioToken,
+        Opcion,
+        Pregunta,
+        RespuestaUsuario,
+        Rol,
+        Token,
+        Usuario,
+    ],
 });
 
 export default db;
